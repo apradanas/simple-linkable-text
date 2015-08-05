157aeb2b2f426e538628f9933239745a58475c45
apradanas <apradanas@gmail.com>
2015-08-05 14:40:53 +0700

    Add LinkModifier and Range util

---
+++ library/src/main/java/com/apradanas/simplelinkabletext/util/Range.java
@@ -0,0 +1,14 @@
+package com.apradanas.simplelinkabletext.util;
+
+/**
+ * Created by @apradanas
+ */
+public class Range {
+    public int start;
+    public int end;
+
+    public Range(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+}
